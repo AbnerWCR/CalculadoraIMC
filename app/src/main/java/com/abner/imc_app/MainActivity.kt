8278030f69b9ac0d0f7c509ecdfa030e0a33eeca
AbnerWCR <abnerwcrodrigues@gmail.com>
2021-11-02 14:16:21 -0300

    iniciando a lóigica para calculo a partir do evento de click no botão

--- app/src/main/java/com/abner/imc_app/MainActivity.kt
+++ app/src/main/java/com/abner/imc_app/MainActivity.kt
@@ -2,43 +2,37 @@ package com.abner.imc_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.w("lifecycle", "entrei no onCreate - iniciando aplicação")
-        //finish() -- caso em que o ciclo não é respeitado pois vai do onCreate() --> onDestroy()
+        setListeners()
     }
 
-    override fun onStart() {// criação dos componentes na tela
-        super.onStart()
-        Log.w("lifecycle", "entrei no onStart - criando componentes")
+    private fun setListeners(){
+        alturaEDT?.doAfterTextChanged { text ->
+            //Toast.makeText(this, text.toString(), Toast.LENGTH_SHORT).show()
+        }
+        pesoEDT?.doOnTextChanged { text, _, _, _ ->
+            //tituloTXT?.text = text
+        }
+        calcularBTN?.setOnClickListener{
+            calcularIMC(pesoEDT.toString(), alturaEDT.toString())
+        }
     }
 
-    override fun onResume() {//autorização para utilizar a aplicaçãp
-        super.onResume()
-        Log.w("lifecycle", "entrei no onResume - app pornto para uso")
-    }
+    private fun calcularIMC(peso: String, altura: String) {
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
 
-    override fun onPause() {//foco perdido - o usuario deixou a aplicação
-        super.onPause()
-        Log.w("lifecycle", "entrei no onPause - usuario deixou a aplicação")
-    }
-
-    override fun onStop() {// tela parada - não esta mais ativa, necessita de um onRestart
-        super.onStop()
-        Log.w("lifecycle", "entrei no onStop - aplicação não está mais ativa, necessita de um onRestart")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.w("lifecycle", "entrei no onRestart - aplicação voltando")
-    }
-
-    override fun onDestroy() {//aplicação destruida, para voltar ao app necessita de um onCreate
-        super.onDestroy()
-        Log.w("lifecycle", "entrei no onDestroy - aplicação destruida, para retornar necessita de um onCreate")
+        if (peso != null && altura != null) {
+            val imc = peso / (altura * altura)
+            tituloTXT.text = "Seu IMC é \n%.2f)".format(imc)
+        }
     }
 }
